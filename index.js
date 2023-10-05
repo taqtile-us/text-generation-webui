86dc3cc8aaf4a502e129d8c3dcb7ee39fd01f7ba
eugenos-programos <elena.samchuk.73@mail.ru>
2023-10-05 12:43:44 +0300

    feat: added pdf parser and chunk splitter

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+// import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+// import { HuggingFaceTransformersEmbeddings } from "langchain/embeddings/hf_transformers";
+// import { Chroma } from "langchain/vectorstores/chroma";
 import { PromptTemplate } from "langchain/prompts";
 import { Ollama } from "langchain/llms/ollama";
 import { LLMChain } from "langchain/chains";
-import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-const loader = new PDFLoader('./documents/WMF1000manual.pdf');
-const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 500,
-    chunkOverlap: 0
-});
-const documents = await loader.load();
-const splittedDocs = await textSplitter.splitDocuments(documents);
-console.log(splittedDocs);
+// const docs = new PDFLoader('./documents/WMF1000manual.pdf');
+// const data = await docs.load();
+// const textSplitter = new RecursiveCharacterTextSplitter({
+//     chunkSize: 500,
+//     chunkOverlap: 0,
+// });
+// const splitDocs = await textSplitter.splitDocuments(data);
+// const embeddingModel = new HuggingFaceTransformersEmbeddings({
+//     modelName: "Xenova/all-MiniLM-L6-v2",
+// });
+// const vectorStore = await Chroma.fromDocuments(splitDocs, embeddingModel, {
+//     collectionName: "a-test-collection",
+//     collectionMetadata: {
+//         "hnsw:space": "cosine",
+//     },
+// });
 const template = `use this peace of text: {context}, and answer this question: {question}`;
 const promptTemplate = new PromptTemplate({
     template,
@@ -22,7 +32,7 @@ const model = new Ollama({
 });
 const chain = new LLMChain({
     llm: model,
-    prompt: promptTemplate,
+    prompt: promptTemplate
 });
 const result = await chain.call({
     context: `The hot water boiler and the steam boiler are equipped with
@@ -35,6 +45,6 @@ const result = await chain.call({
     cuit.
      The third safety level is the safety valve. It releases the
     overpressure that is built up.`,
-    question: 'how many protection levels has this boiler?'
+    question: 'what is the temperature of hot water'
 });
 console.log(result);
--- index.ts
+++ index.ts
@@ -1,53 +1,47 @@
-// import {PDFLoader} from "langchain/document_loaders/fs/pdf";
-// import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
-// import {HuggingFaceTransformersEmbeddings} from "langchain/embeddings/hf_transformers";
-// import {Chroma} from "langchain/vectorstores/chroma";
-// import {PromptTemplate} from "langchain/prompts";
+import {PromptTemplate} from "langchain/prompts";
 import {Ollama} from "langchain/llms/ollama";
-// import {RetrievalQAChain} from "langchain/chains";
-// import {getEmbeddingContextSize} from "langchain/dist/base_language/count_tokens";
+import { LLMChain } from "langchain/chains";
+import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
+import {RecursiveCharacterTextSplitter} from 'langchain/text_splitter';
 
-// const docs = new PDFLoader('./documents/WMF1000manual.pdf');
+const loader = new PDFLoader('./documents/WMF1000manual.pdf');
+const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 0
+})
+const documents = await loader.load();
+const splittedDocs = await textSplitter.splitDocuments(documents)
+console.log(splittedDocs);
 
-// const data = await docs.load();
-//
-// const textSplitter = new RecursiveCharacterTextSplitter({
-//     chunkSize: 500,
-//     chunkOverlap: 0,
-// });
-//
-// const splitDocs = await textSplitter.splitDocuments(data);
-//
-// const embeddingModel = new HuggingFaceTransformersEmbeddings({
-//     modelName: "Xenova/all-MiniLM-L6-v2",
-// });
-//
-// const vectorStore = await Chroma.fromDocuments(splitDocs, embeddingModel, {
-//     collectionName: "a-test-collection",
-//     collectionMetadata: {
-//         "hnsw:space": "cosine",
-//     },
-// });
-//
-// const template = `Use the following pieces of context to answer the question at the end.
-// If you don't know the answer, just say that you don't know, don't try to make up an answer.
-// Use three sentences maximum and keep the answer as concise as possible.
-// Always say "thanks for asking!" at the end of the answer.
-// {context}
-// Question: {question}
-// Helpful Answer:`;
+const template = `use this peace of text: {context}, and answer this question: {question}`;
+
+const promptTemplate = new PromptTemplate({
+    template,
+    inputVariables: ['context', 'question']
+})
 
 const model = new Ollama({
-    baseUrl: "http://192.168.1.183:11435",
+    baseUrl: "http://localhost:11434",
     model: "llama2:13b",
 });
 
-// const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), {
-//     prompt: PromptTemplate.fromTemplate(template),
-// });
-//
-// const result = await chain.call({query: 'what is the water tank capacity of this coffee machine?'})
+const chain = new LLMChain({
+    llm: model,
+    prompt: promptTemplate,
+});
 
-const answer = await model.call('what is the capital of new zeeland')
+const result = await chain.call({
+    context: `The hot water boiler and the steam boiler are equipped with
+    three-level protection from overheating and bursting.
+     The first level is the temperature sensor. It switches the
+    heater on and off based on the water temperature.
+     The second level is the safety thermostat. If the surface
+    temperature of a hot water boiler / steam boilerexceeds
+    155°C, the safety thermostat interrupts the heating cir-
+    cuit.
+     The third safety level is the safety valve. It releases the
+    overpressure that is built up.`,
+    question: 'how many protection levels has this boiler?'
+})
 
-console.log(answer)
+console.log(result)
