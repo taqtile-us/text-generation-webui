c3a60117bca59dd6185ccfb59d59f03d780fa1cb
eugenos-programos <elena.samchuk.73@mail.ru>
2023-10-06 14:30:28 +0300

    feat: fixed llm request

---
+++ next-js-ssr-client/src/app/page.js
@@ -0,0 +1,92 @@
+"use client";
+import { useState } from 'react';
+import logo from '../assets/images/Taqtile_Vertical_Black-removebg-preview.png';
+export default function Home() {
+    const [isLoaded, setIsLoaded] = useState(true);
+    const [prompt, setPrompt] = useState('');
+    const [chatMessages, setChatMessages] = useState(['Hi! My name is ChatGuru! I am your personal AI assistant! How can I help you?', 'User\'s request will be here', 'ChatGuru response will be here']);
+    const [selectedManualPath, setSelectedManualPath] = useState('../assets/manuals/WMF1000manual.pdf');
+    const [disableNewRequest, setDisableNewRequest] = useState(false);
+    const [splittedDocs, setSplittedDocs] = useState();
+    const onAsk = async () => {
+        const response = await fetch('api/pdf', {
+            method: 'POST',
+            body: JSON.stringify({ filepath: '../assets/manuals/WMF1000manual.pdf' }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        setChatMessages(prev => [...prev, JSON.stringify(response)]);
+    };
+    return (<div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100vh',
+            width: '100vw',
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#b9b9b9',
+            position: 'relative',
+            fontFamily: 'Helvetica',
+            color: '#000000'
+        }}>
+
+          <div style={{ position: 'absolute', top: '32px', right: '32px' }}>
+              <h3>Select Instruction</h3>
+              <div>{JSON.stringify(splittedDocs)}</div>
+              <select style={{
+            borderRadius: '8px',
+            border: 'none',
+            height: '32px',
+            minWidth: '256px',
+            fontSize: '14px',
+            padding: '8px'
+        }} defaultValue={selectedManualPath}>
+                  <option value={'../assets/manuals/WMF1000manual.pdf'}>Coffee Machine user manual</option>
+                  <option value={'../assets/manuals/air-condition-manual.pdf'}>Air Conditioner service manual</option>
+              </select>
+          </div>
+
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+              <img height={'128px'} src={logo.src}/>
+              <h1>ChatGuru</h1>
+          </div>
+          {isLoaded
+            ? (<div style={{ height: '75%', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', rowGap: '32px' }}>
+
+                      <div style={{ width: '50%', display: 'flex', flexDirection: "column", rowGap: '16px' }}>
+                          {chatMessages.map((message, index) => {
+                    return <div style={{ display: 'flex', maxWidth: '75%', borderRadius: '12px', backgroundColor: index % 2 ? 'rgba(238,127,49,0.75)' : 'rgba(0,0,0,0.25)', alignSelf: index % 2 ? 'flex-end' : 'flex-start', padding: '8px 12px 8px 12px', color: '#FFFFFF' }}>
+                                          {message}
+                                     </div>;
+                })}
+                      </div>
+
+                      <div style={{ display: 'flex', columnGap: '12px', width: '50%' }}>
+                          <input onSubmit={e => e.preventDefault()} placeholder='Enter your prompt here' style={{
+                    fontSize: '16px',
+                    padding: '0 12px 0 12px',
+                    backgroundColor: '#fffffe',
+                    border: '0px',
+                    borderRadius: '5px',
+                    height: '36px',
+                    width: '100%'
+                }} type={'text'} value={prompt} onChange={e => setPrompt(e.currentTarget.value)}/>
+                          <button onClick={() => onAsk()} disabled={disableNewRequest} style={{
+                    color: '#FFFFFF',
+                    backgroundColor: disableNewRequest ? '#6a3e1e' : '#ee7f31',
+                    border: '0px',
+                    borderRadius: '5px',
+                    height: '40px',
+                    width: '64px',
+                    fontSize: '16px',
+                    fontWeight: "bold",
+                    cursor: "pointer"
+                }}>
+                              {disableNewRequest ? '...' : 'Ask'}
+                          </button>
+                      </div>
+                  </div>)
+            : <div>Loading</div>}
+      </div>);
+}
---
+++ next-js-ssr-client/src/pages/api/pdf/index.js
@@ -0,0 +1,39 @@
+import { PromptTemplate } from "langchain/prompts";
+import { Ollama } from "langchain/llms/ollama";
+import { RetrievalQAChain } from "langchain/chains";
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { HuggingFaceTransformersEmbeddings } from "langchain/embeddings/hf_transformers";
+export default async function uploadPDF(req, res) {
+    if (req.method === 'POST') {
+        const { filepath } = await JSON.parse(req.body);
+        const loader = new PDFLoader(filepath);
+        const textSplitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 500,
+            chunkOverlap: 0
+        });
+        const documents = await loader.load();
+        const splittedDocs = await textSplitter.splitDocuments(documents);
+        console.log(splittedDocs);
+        const embeddingModel = new HuggingFaceTransformersEmbeddings({
+            modelName: 'Xenova/all-MiniLM-L6-V2'
+        });
+        const vectorStore = await MemoryVectorStore.fromDocuments(splittedDocs, embeddingModel);
+        const template = `use this peace of text: {context}, and answer this question: {question}`;
+        const promptTemplate = new PromptTemplate({
+            template,
+            inputVariables: ['context', 'question']
+        });
+        const model = new Ollama({
+            baseUrl: "http://localhost:11434",
+            model: "llama2:13b",
+        });
+        const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), { prompt: promptTemplate });
+        const result = await chain.call({
+            query: 'what is this instruction about'
+        });
+        res.send({ data: result });
+        res.status(200);
+    }
+}
